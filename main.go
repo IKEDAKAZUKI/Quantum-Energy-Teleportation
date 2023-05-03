dd423080b874126dc922f76809c9e02afc9c99f6
Andrew Snodgrass <git@pointlander.info>
2023-05-03 10:12:05 -0500

    Make music

--- main.go
+++ main.go
@@ -1,27 +1,41 @@
 package main
 
 import (
+	"io/ioutil"
+	"strings"
+
 	"gitlab.com/gomidi/midi/writer"
 )
 
 func main() {
+	data, _ := ioutil.ReadFile("data.bits")
+	lines := strings.Split(string(data), "\n")
+
 	err := writer.WriteSMF("notes.mid", 2, func(wr *writer.SMF) error {
-		wr.SetChannel(0)
-		writer.NoteOn(wr, 50, 100)
-		wr.SetDelta(120)
-		writer.NoteOff(wr, 50)
+		for _, line := range lines {
+			bits := []byte(line)
+			if len(bits) != 2 {
+				continue
+			}
+			wr.SetChannel(0)
+			if bits[0] == '1' {
+				writer.NoteOn(wr, 50, 100)
+			}
+			if bits[1] == '1' {
+				writer.NoteOn(wr, 53, 100)
+			}
+			wr.SetDelta(120)
+			if bits[0] == '1' {
+				writer.NoteOff(wr, 50)
+			}
+			if bits[1] == '1' {
+				writer.NoteOff(wr, 53)
+			}
+			wr.SetDelta(240)
+		}
 
-		wr.SetDelta(240)
-		writer.NoteOn(wr, 53, 100)
-		wr.SetDelta(20)
-		writer.NoteOff(wr, 53)
 		writer.EndOfTrack(wr)
 
-		wr.SetChannel(1)
-		writer.NoteOn(wr, 50, 100)
-		wr.SetDelta(60)
-		writer.NoteOff(wr, 50)
-		writer.EndOfTrack(wr)
 		return nil
 	})
 	if err != nil {
