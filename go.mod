3b8803f028e48696e852c74c127429ec527c6645
Andrew Snodgrass <git@pointlander.info>
2023-05-04 20:17:45 -0500

    Added histograms

--- go.mod
+++ go.mod
@@ -2,4 +2,15 @@ module github.com/pointlander/Quantum-Energy-Teleportation
 
 go 1.18
 
-require gitlab.com/gomidi/midi v1.23.7 // indirect
+require (
+	git.sr.ht/~sbinet/gg v0.3.1 // indirect
+	github.com/ajstarks/svgo v0.0.0-20211024235047-1546f124cd8b // indirect
+	github.com/go-fonts/liberation v0.2.0 // indirect
+	github.com/go-latex/latex v0.0.0-20210823091927-c0d11ff05a81 // indirect
+	github.com/go-pdf/fpdf v0.6.0 // indirect
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
+	gitlab.com/gomidi/midi v1.23.7 // indirect
+	golang.org/x/image v0.0.0-20220902085622-e7cb96979f69 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	gonum.org/v1/plot v0.12.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
 	"strings"
 
 	"gitlab.com/gomidi/midi/writer"
+	"gonum.org/v1/plot"
+	"gonum.org/v1/plot/plotter"
+	"gonum.org/v1/plot/vg"
 )
 
 func main() {
@@ -42,12 +46,21 @@ func main() {
 		panic(err)
 	}
 
+	values := make(map[string]plotter.Values)
+	v := plotter.Values{}
 	err = writer.WriteSMF("markov.mid", 1, func(wr *writer.SMF) error {
 		last := ""
 		for _, line := range lines {
 			bits := []byte(line)
 			if len(bits) != 2 {
 				continue
+			}
+			i := float64((bits[1] - '0') + 2*(bits[0]-'0'))
+			v = append(v, i)
+			if last == "" {
+				values["00"] = append(values["00"], i)
+			} else {
+				values[last] = append(values[last], i)
 			}
 			if last == "" || last == "00" {
 				wr.SetDelta(120)
@@ -108,6 +121,41 @@ func main() {
 
 		return nil
 	})
+	if err != nil {
+		panic(err)
+	}
+
+	for key, value := range values {
+		p := plot.New()
+		if err != nil {
+			panic(err)
+		}
+		p.Title.Text = "histogram plot"
+
+		histogram, err := plotter.NewHist(value, 4)
+		if err != nil {
+			panic(err)
+		}
+		p.Add(histogram)
+
+		err = p.Save(8*vg.Inch, 8*vg.Inch, fmt.Sprintf("%s_historgram.png", key))
+		if err != nil {
+			panic(err)
+		}
+	}
+	p := plot.New()
+	if err != nil {
+		panic(err)
+	}
+	p.Title.Text = "histogram plot"
+
+	histogram, err := plotter.NewHist(v, 4)
+	if err != nil {
+		panic(err)
+	}
+	p.Add(histogram)
+
+	err = p.Save(8*vg.Inch, 8*vg.Inch, "historgram.png")
 	if err != nil {
 		panic(err)
 	}
